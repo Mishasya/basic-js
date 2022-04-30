23aa375b07526bee64556d3e71e7c5e05d063013
Mishasya <Mishasya@bk.ru>
2022-04-30 18:34:58 +0300

    feat: add solution matrix-elements-sum

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -17,7 +17,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * The result should be 9
  */
 function getMatrixElementsSum(matrix) {
+  let copyMatrix = [...matrix]
+  let arr = []
+  let sum = 0
   
+  for(let i = 0; i < copyMatrix.length; i++) {
+    for(let j = 0; j < copyMatrix[i].length; j++) {
+      if(arr.includes(j)) {
+        
+      } else {
+          if(copyMatrix[i][j] === 0) 
+          arr.push(j)
+          sum += copyMatrix[i][j]
+        }
+      
+    }
+  }
+  return sum
 }
 
 module.exports = {
